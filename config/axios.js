4e63e1582b7faf447fe391cbed6626b922385c49
Oscar Villanueva <javiondo68@gmail.com>
2020-05-12 18:01:16 -0500

    passwords and url in env

--- config/axios.js
+++ config/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "https://dev-topstore.pantheonsite.io/wp-json/wc/v3",
+    baseURL: process.env.COMMERCE_API,
     auth: {
-        username: "ck_585f4a010018c5f8df53c6cb36d4dedcb83663ba",
-        password: "cs_c581aa162f7857daa7bda08ba35eca19821dac0d"
+        username: process.env.COMSUMER_KEY,
+        password: process.env.CONSUMER_SECRET
     }
 })
 
